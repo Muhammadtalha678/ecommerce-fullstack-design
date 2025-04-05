b37084db22947dc85fe4c70df055a95455cba1d5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-05 17:26:31 +0500

    create navbar link file

---
+++ frontend/src/lib/navbarLinks.ts
@@ -0,0 +1,6 @@
+export const navbarLinks = [
+    { name: 'Hot offers', path: '/hot-offers' },
+    { name: 'Gift boxes', path: '/gift-boxes' },
+    { name: 'Projects', path: '/projects' },
+    { name: 'Menu item', path: '/menu-items' }
+]
