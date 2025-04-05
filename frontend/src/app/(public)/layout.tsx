148319dbe6de6870b683b3452b55fe0081ada67b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-05 18:16:06 +0500

    add header in public route

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/common/Header/Header'
 import React from 'react'
 
 const PublicLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <div>
+            <Header />
             {children}
         </div>
     )
