ba0054cad85a745a49c2b66cdeb9e9244fbb8b3a
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-05 17:25:21 +0500

    create top navbar link file

---
+++ frontend/src/lib/topNavLinks.ts
@@ -0,0 +1,8 @@
+import { FaUser, FaComment, FaHeart, FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
+
+export const topNavLink = [
+    { name: 'Profile', path: '/profile', icons: FaUser },
+    { name: 'Message', path: '/message', icons: FaComment },
+    { name: 'Orders', path: '/orders', icons: FaHeart },
+    { name: 'Cart', path: '/cart', icons: FaShoppingCart }
+]
