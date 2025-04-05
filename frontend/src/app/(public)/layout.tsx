096c49c199e2ee5e21bfef5696efe06e67ae8aaa
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-06 01:21:43 +0500

    add secondary navbar in public layout

--- frontend/src/app/(public)/layout.tsx
+++ frontend/src/app/(public)/layout.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/common/Footer/Footer'
 import Header from '@/components/common/Header/Header'
+import TopNav from '@/components/common/Header/Navbar'
 import React from 'react'
 
 const PublicLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <div>
             <Header />
+            <TopNav />
             {children}
             <Footer />
         </div>
