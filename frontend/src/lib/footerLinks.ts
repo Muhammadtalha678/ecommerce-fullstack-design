ab79538f98f609d1cb03bf4b05c647cc63f405db
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-05 17:27:03 +0500

    create top footer links file

---
+++ frontend/src/lib/footerLinks.ts
@@ -0,0 +1,6 @@
+export const FooterLinks = [
+    { title: 'About', links: ['About Us', 'Find Store', 'Categories', 'Blogs'] },
+    { title: 'Partnership', links: ['About Us', 'Find Store', 'Categories', 'Blogs'] },
+    { title: 'Information', links: ['Help Center', 'Money Refund', 'Shipping', 'Contact Us'] },
+    { title: 'For Users', links: ['Login', 'Register', 'Settings', 'My Orders'] },
+]
