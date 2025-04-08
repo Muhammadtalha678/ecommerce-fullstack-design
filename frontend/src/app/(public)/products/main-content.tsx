ef9555bc157598327d932a597a9a9e344f50f4c9
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-08 12:51:09 +0500

    product page complete

--- frontend/src/app/(public)/products/main-content.tsx
+++ frontend/src/app/(public)/products/main-content.tsx
@@ -2,16 +2,88 @@
 import React, { useState } from 'react';
 import FiltersViewToggle from '@/components/product/FiltersViewToggle';
 import Sidebar from '@/components/product/Sidebar';
+import ProductCard from '@/components/product/ProductCard';
+import ProductListing from '@/components/product/ProductListing';
+import { Product } from '@/interfaces/Product';
 
 const MainContent = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
-  // const [view, setView] = useState<'grid' | 'list'>('grid'); // State for view toggle
-  const [view, setView] = useState<'grid' | 'list'>('grid')
+
+  const products: Product[] = [
+    {
+      id: 1,
+      title: 'Canon Cmera EOS 2000, Black 10x zoom',
+      image: '/images/deal-1.jpg',
+      rating: 4,
+      orders: 8112800,
+      price: 998.00,
+      originalPrice: 1128.00,
+      shipping: 'Free Shipping',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+      id: 2,
+      title: 'GoPro HERO6 4K Action Camera - Black',
+      image: '/images/deal-2.jpg',
+      rating: 4,
+      orders: 154,
+      price: 998.00,
+      shipping: 'Free Shipping',
+      description:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    },
+    {
+      id: 3,
+      title: 'GoPro HERO6 4K Action Camera - Black',
+      image: '/images/deal-3.jpg',
+      rating: 4,
+      orders: 154,
+      price: 998.00,
+      shipping: 'Free Shipping',
+      description:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    },
+    {
+      id: 4,
+      title: 'GoPro HERO6 4K Action Camera - Black',
+      image: '/images/deal-4.jpg',
+      rating: 4,
+      orders: 154,
+      price: 998.00,
+      shipping: 'Free Shipping',
+      description:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    },
+    {
+      id: 5,
+      title: 'GoPro HERO6 4K Action Camera - Black',
+      image: '/images/cat16.jpg',
+      rating: 4,
+      orders: 154,
+      price: 998.00,
+      originalPrice: 1128.00,
+      shipping: 'Free Shipping',
+      description:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    },
+    {
+      id: 6,
+      title: 'GoPro HERO6 4K Action Camera - Black',
+      image: '/images/cat21.jpg',
+      rating: 4,
+      orders: 154,
+      price: 998.00,
+      shipping: 'Free Shipping',
+      description:
+        'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    },
+  ];
   return (
     <div className="px-4 sm:px-6 md:px-10 lg:px-16 xl:px-24">
       <div className="grid grid-cols-12 gap-4">
         {/* Filter Button - only on small screens */}
-        <div className="col-span-6 lg:hidden">
+        <div className="col-span-6 lg:hidden mt-2">
           <button
             className="px-3 py-2 text-sm bg-gray-100 border border-gray-300 rounded-md shadow-sm hover:bg-gray-200 transition"
             onClick={() => setSidebarOpen(true)}
@@ -24,15 +96,7 @@ const MainContent = () => {
         <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
         {/* Product Listing Section */}
-        <div className="col-span-12 lg:col-span-9 py-6">
-          <FiltersViewToggle view={view} setView={setView} />
-          {/* Placeholder for ProductListing */}
-          <div className="mt-4">
-            <p className="text-sm text-gray-600">
-
-            </p>
-          </div>
-        </div>
+        <ProductListing products={products} />
       </div>
     </div>
   );
