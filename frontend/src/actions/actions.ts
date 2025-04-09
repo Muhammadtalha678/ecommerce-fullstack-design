6171f5d4ccb44b62e0115ec0c87258300dfe3c21
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-09 22:29:30 +0500

    debug the data

--- frontend/src/actions/actions.ts
+++ frontend/src/actions/actions.ts
@@ -1,4 +1,10 @@
 'use server'
 export const handleRegister = async (formData: FormData) => {
+    const registerData = {
+        fullname: formData.get('name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    }
+    console.log(registerData);
 
 }
