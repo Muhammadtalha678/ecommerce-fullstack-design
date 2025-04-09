1025ec6a5bcf310bad67a567121226c556530ba3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-09 17:04:16 +0500

    add the cors to allow frontend url,methods etc

--- backend-express/src/index.js
+++ backend-express/src/index.js
@@ -2,8 +2,18 @@ import express from 'express'
 import { env } from './lib/configs/env.config.js'
 import { connectDb } from './lib/db/connectDb.js'
 import AuthRouter from './routers/auth.router.js'
+import cors from 'cors'
 const app = express()
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], //allow all methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow cookies if needed
+}))
+
 app.use(express.json())
+
 app.get('/', (req, res) => {
     return res.send('Hello World')    
 })
