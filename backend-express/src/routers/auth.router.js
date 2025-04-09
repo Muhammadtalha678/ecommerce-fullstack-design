fa17fe6002e6a5a789a02b2e1afdb21d6066f489
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-09 16:51:42 +0500

    change the parameter

--- backend-express/src/routers/auth.router.js
+++ backend-express/src/routers/auth.router.js
@@ -3,7 +3,7 @@ import { validateRequest } from '../middlewares/validateRequest.middleware.js'
 import {registerValidation} from '../lib/validations/auth.validation.js'
 const router = express.Router()
 
-router.post('/register', validateRequest(registerValidation), (res, req) => {
+router.post('/register', validateRequest(registerValidation), (req, res) => {
     return res.send(req.body)
 })
 
