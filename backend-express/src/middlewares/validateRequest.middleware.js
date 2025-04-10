2d62c464d20ddbe2e767697ac912a34bc029e0a2
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-10 19:55:14 +0500

    change the error response

--- backend-express/src/middlewares/validateRequest.middleware.js
+++ backend-express/src/middlewares/validateRequest.middleware.js
@@ -2,6 +2,6 @@ import { sendResponse } from "../helpers/sendResponse.js"
 
 export const validateRequest = (validationSchema) => (req, res, next) => {
     const { error } = validationSchema.validate(req.body)
-    if (error) return sendResponse(res,400,true,error.message,null)
+    if (error) return sendResponse(res,400,true,error,null)
     next()
 }
