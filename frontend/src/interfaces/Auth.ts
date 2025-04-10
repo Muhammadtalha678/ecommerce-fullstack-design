6d0ebb75ab64c31ffbd34e15db3e3dccbf272e56
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-11 00:36:08 +0500

    interface for auth response

---
+++ frontend/src/interfaces/Auth.ts
@@ -0,0 +1,10 @@
+export interface ApiResponse {
+    error: boolean, errors: {
+        fullname?: string,
+        email?: string,
+        password?: string,
+        confirmPassword?: string,
+        [key: string]: string | undefined; // for any extra fields
+    },
+    data: any | null
+} 
