cc7c421e9255ef225ab0f39e52297c514dd8bcda
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-11 01:21:01 +0500

    edit  dev and prod url

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -1,7 +1,7 @@
 
-// const DEV_URL = "http://localhost:5000/"
+const DEV_URL = "http://localhost:5000"
 const PROD_URL = process.env.PROD_URL
-
+const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
 export const ApiRoutes = {
-    register: PROD_URL + '/api/auth/register'
+    register: BASE_URL + '/api/auth/register'
 }
