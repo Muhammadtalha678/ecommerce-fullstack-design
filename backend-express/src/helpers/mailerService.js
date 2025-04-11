5f760865065dcf29fc8f747ff76f94f65c4af793
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-11 19:53:50 +0500

    change the mail message

--- backend-express/src/helpers/mailerService.js
+++ backend-express/src/helpers/mailerService.js
@@ -2,19 +2,19 @@ import * as nodemailer from 'nodemailer'
 import { env } from '../lib/configs/env.config.js'
 const sendVerificationEmail = async (email, token) => {
     const mailOptions = {
-        from: `DevHubInternship program "${env.EMAIL_USER}"`, // Sender address
+        from: `DevHubInternship program`, // Sender address
             to: email, // Recipient address
             subject: 'Verify Your Email', // Subject line
             html: `
               <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
-                <h2>Welcome to Hackathon Website!</h2>
+                <h2>Welcome to DevHubInternship Internship program Website!</h2>
                 <p>Dear User,</p>
                 <p>Thank you for registering on our platform. Please use the following OTP to complete your registration:</p>
                 <h3 style="background: #f4f4f4; padding: 10px; border-radius: 5px; text-align: center;">
                   ${token}
                 </h3>
                 <p>If you did not register, please ignore this email.</p>
-                <p>Best regards,<br>Hackathon Website Team</p>
+                <p>Best regards,<br>DevHubInternship Internship program</p>
               </div>
             `, 
     }
