ead148e68cf248c3c920345481cdbdfd64c89823
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-12 01:47:16 +0500

    create verify email api route

--- backend-express/src/routers/auth.router.js
+++ backend-express/src/routers/auth.router.js
@@ -2,9 +2,10 @@ import express from 'express'
 import { validateRequest } from '../middlewares/validateRequest.middleware.js'
 import {registerValidation} from '../lib/validations/auth.validation.js'
 
-import { registerController } from '../controllers/auth.controller.js'
+import { registerController, verifyEmailController } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register', validateRequest(registerValidation), registerController)
+router.post('/verify-email', verifyEmailController)
 
 export default router
