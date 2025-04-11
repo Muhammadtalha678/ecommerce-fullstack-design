e723f9d7cf83410b1111dd90d6d2f41865e1cdfe
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-12 02:49:32 +0500

    add toaster on succces or general failure

--- frontend/src/app/(auth)/verify-email/VerfiyEmailForm.tsx
+++ frontend/src/app/(auth)/verify-email/VerfiyEmailForm.tsx
@@ -1,10 +1,32 @@
 'use client'
 import { verifyEmailAction } from '@/actions/actions'
 import VerifyEmail from '@/components/Auth/VerifyEmail'
-import React, { useActionState } from 'react'
-
+import React, { useActionState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import toast from 'react-hot-toast'
 const VerfiyEmailForm = () => {
+    const router = useRouter()
     const [state, verifyAction, pending] = useActionState(verifyEmailAction, undefined)
+    useEffect(() => {
+        if (state) {
+            if (state.errors?.general) {
+                toast.error(state.errors.general)
+            } else {
+                if (!state.error) {
+                    toast.success(state.data?.message || "Registration Successfull!")
+                    const email = state.data?.email;
+                    if (email) {
+                        setTimeout(() => {
+                            // router.push(`/verify-email?email=${encodeURIComponent(email)}`);
+                        }, 2000);
+                    }
+
+                }
+
+            }
+
+        }
+    }, [state, router])
     return (
         <VerifyEmail action={verifyAction} state={state} pending={pending} />
     )
