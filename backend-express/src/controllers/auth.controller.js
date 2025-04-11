47125e19deb519ced7451fc2158fa1cf16e9905b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-11 19:20:47 +0500

    add the send verification of email

--- backend-express/src/controllers/auth.controller.js
+++ backend-express/src/controllers/auth.controller.js
@@ -1,23 +1,33 @@
 import UserModal from '../modals/user.modal.js'
 import { sendResponse } from '../helpers/sendResponse.js'
 import bcrypt from 'bcrypt'
+import { sendVerificationEmail } from '../helpers/mailerService.js'
 const registerController = async (req, res) => {
     try {
         let { fullname, email, password } = req.body
         
-        console.log(fullname);
         // find user
         const user = await UserModal.findOne({email})
-        console.log(user);
         if (user) {
             return sendResponse(res,409,true,{email:"User already registered"},null)
         }
+
+        // 6-digit OTP generate karna
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
         const hashPassword = await bcrypt.hash(password, 10)
         password = hashPassword
         let newUSer = new UserModal({ fullname, email, password })
         newUSer.save()
-        
-        return sendResponse(res,200,false,{},{message:"User Registered Succesfully"})
+        try {
+            await sendVerificationEmail(email,verificationToken)
+            
+        } catch (emailError) {
+            // Rollback user creation if email sending fails
+            await UserModal.findByIdAndDelete(newUSer._id)
+             return sendResponse(res, 500, true, { general: "Failed to send verification email. Please try again." }, null);
+        }
+        return sendResponse(res,200,false,{},{message:"User Registered Succesfully, OTP sent to email."})
         
     } catch (error) {
         
