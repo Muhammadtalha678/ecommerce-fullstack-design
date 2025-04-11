724ced6370355a053673666e26dacc2e8ee5f0cf
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-12 01:46:41 +0500

    create email verifiation action

--- frontend/src/actions/actions.ts
+++ frontend/src/actions/actions.ts
@@ -49,3 +49,48 @@ export const register = async (state: ApiResponse | undefined, formData: FormDat
     }
 
 }
+
+
+
+export const verifyEmailAction = async (state: ApiResponse | undefined, formData: FormData): Promise<ApiResponse> => {
+    const verifyEmailData = {
+        otp: formData.get('otp'),
+        email: formData.get('email')
+    }
+    try {
+        const controller = new AbortController()
+        const timeoutId = setTimeout(() => controller.abort(), 10000);
+        const response = await fetch(ApiRoutes.verfiyEmail, {
+            signal: controller.signal, // ✅ Needed for timeout to work
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: "POST",
+            body: JSON.stringify(verifyEmailData)
+        })
+        clearTimeout(timeoutId)
+        const data = await response.json()
+        if (!response.ok) {
+            return {
+                error: true,
+                errors: data.errors || {},
+                data: null,
+            };
+        }
+
+        return {
+            error: false, errors: {}, data: data.data
+        }
+    } catch (error) {
+        const err = error as Error
+        const timeout = err.name === "AbortError";
+
+        return {
+            error: true,
+            errors: {
+                general: timeout ? "Request timed out" : err.message || "An error occurred",
+            },
+            data: null,
+        };
+    }
+}
