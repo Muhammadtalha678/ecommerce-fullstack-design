3fc5b27d2e8cb44bd3b26147bf3bce062f57e9c6
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 00:55:28 +0500

    add a login api route

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -4,6 +4,7 @@ const PROD_URL = process.env.PROD_URL
 const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
 export const ApiRoutes = {
     register: BASE_URL + '/api/auth/register',
+    login: BASE_URL + '/api/auth/login',
     verfiyEmail: BASE_URL + '/api/auth/verify-email',
     resendEmail: BASE_URL + '/api/auth/resend-verification',
 }
