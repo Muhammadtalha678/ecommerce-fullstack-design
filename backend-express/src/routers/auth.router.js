65dd23d9bd57406ab2213a24eeac801b7b2a561a
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 00:54:47 +0500

    create a login api route

--- backend-express/src/routers/auth.router.js
+++ backend-express/src/routers/auth.router.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { validateRequest } from '../middlewares/validateRequest.middleware.js'
-import {registerValidation} from '../lib/validations/auth.validation.js'
+import {loginValidation, registerValidation} from '../lib/validations/auth.validation.js'
 
-import { registerController, resendEmailController, verifyEmailController } from '../controllers/auth.controller.js'
+import { loginController, registerController, resendEmailController, verifyEmailController } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register', validateRequest(registerValidation), registerController)
+router.post('/login', validateRequest(loginValidation), loginController)
 router.post('/verify-email', verifyEmailController)
 router.post('/resend-verification', resendEmailController)
 
