8ca8382d03e9302bc56e2d774b94fa417f5ba2f3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 00:54:19 +0500

    add secret key in config

--- backend-express/src/lib/configs/env.config.js
+++ backend-express/src/lib/configs/env.config.js
@@ -6,5 +6,6 @@ export const env = {
     EMAIL_HOST:process.env.EMAIL_HOST,
     EMAIL_PORT:process.env.EMAIL_PORT,
     EMAIL_USER:process.env.EMAIL_USER,
-    EMAIL_PASS:process.env.EMAIL_PASS,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    AUTH_SECRET:process.env.AUTH_SECRET 
 }
