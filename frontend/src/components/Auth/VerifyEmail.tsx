59e388b2ca6434e7057013899b29f07feb8f2aaf
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 15:39:55 +0500

    add credentials include in request

--- frontend/src/components/Auth/VerifyEmail.tsx
+++ frontend/src/components/Auth/VerifyEmail.tsx
@@ -60,7 +60,8 @@ const VerifyEmail = ({
             const res = await fetch(ApiRoutes.resendEmail, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email })
+                body: JSON.stringify({ email }),
+                credentials: 'include'
             })
 
             const data = await res.json()
