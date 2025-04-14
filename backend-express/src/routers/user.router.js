43f7b3253e3bab7853f1d85bfc2ce623f1872828
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:05:46 +0500

    add user router

--- backend-express/src/routers/user.router.js
+++ backend-express/src/routers/user.router.js
@@ -1,15 +1,7 @@
 import express from 'express'
 import { authenticateUser } from '../middlewares/authentication.middleware'
-import { sendResponse } from '../helpers/sendResponse.js'
+import { userController } from '../controllers/user.controller.js'
 
 const routers = express.Router()
 
-routers.get('/userInfo', authenticateUser, async (req, res) => {
-    try {
-        const {fullname,email,role} = req.body
-        sendResponse(res, 200, false, {},{fullname,email,role})
-    } catch (error) {
-        
-        sendResponse(res, 500, true, {general:"Something went wrong"},null)
-    }
-})
+routers.get('/userInfo', authenticateUser, userController)
