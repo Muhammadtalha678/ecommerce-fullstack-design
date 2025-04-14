a66fcfc6859224bf3b9af43d20058e5df5c4de0f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:45:31 +0500

    remove constant api headers

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -1,4 +1,3 @@
-import Cookies from 'js-cookie'
 const DEV_URL = "http://localhost:5000"
 const PROD_URL = process.env.NEXT_PUBLIC_PROD_URL
 const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
@@ -8,14 +7,6 @@ export const ApiRoutes = {
     verfiyEmail: `${BASE_URL}/api/auth/verify-email`,
     resendEmail: `${BASE_URL}/api/auth/resend-verification`,
     refreshToken: `${BASE_URL}/api/auth/refreshToken`,
-    user: `${BASE_URL}/api/auth/user`,
+    user: `${BASE_URL}/api/user/userInfo`,
 }
 
-export const getHeaders = () => {
-    const token = Cookies.get('token')
-    return ({
-        headers: {
-            Authorization: `Bearer ` + token
-        }
-    })
-}
