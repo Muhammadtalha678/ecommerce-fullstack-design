506ce797ef0d6a92565d0f365a676627b2701f84
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:02:38 +0500

    add user info route

--- backend-express/src/index.js
+++ backend-express/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { env } from './lib/configs/env.config.js'
 import { connectDb } from './lib/db/connectDb.js'
 import AuthRouter from './routers/auth.router.js'
+import UserRouter from './routers/user.router.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 const app = express()
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 connectDb()
     .then(() => {
         app.use('/api/auth',AuthRouter)
+        app.use('/api/user',UserRouter)
         app.listen(env.PORT, () => {
             console.log(`Project runnig on port ${env.PORT}`);
         })
