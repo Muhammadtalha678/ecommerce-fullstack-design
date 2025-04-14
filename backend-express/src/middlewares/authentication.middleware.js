d79baa170187aaecfd945dd6e0c9cfcb487a2817
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:16:43 +0500

    get user info complete

--- backend-express/src/middlewares/authentication.middleware.js
+++ backend-express/src/middlewares/authentication.middleware.js
@@ -1,13 +1,20 @@
-import { env } from "../lib/configs/env.config";
-import {sendResponse} from '../helpers/sendResponse'
+import { env } from "../lib/configs/env.config.js";
+import {sendResponse} from '../helpers/sendResponse.js'
+import UserModal from '../modals/user.modal.js'
 export async function authenticateUser(req, res, next) {
     try {
-        const authHeader = req.headers.authorization
+        const authHeader = req?.headers?.authorization
+        
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
-         return sendResponse(res, 401, true, { general: 'No token provided' }, null);
+            return sendResponse(res, 401, true, { general: 'No token provided' }, null);
         }
-        const token = authHeader.split(' ')[1];
-        const decoded = jwt.verify(token, env.AUTH_SECRET);
+        const token = authHeader?.split(' ')[1];
+        let decoded;
+        try {
+            decoded = jwt.verify(token, env.AUTH_SECRET)
+        } catch(error) {
+            return sendResponse(res,401,true,{general:"Invalid access token"},null)
+        }
         const user = await UserModal.findById(decoded.id).lean();
         if (!user) {
         return sendResponse(res, 404, true, { general: 'User not found' }, null);
--- backend-express/src/routers/user.router.js
+++ backend-express/src/routers/user.router.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authenticateUser } from '../middlewares/authentication.middleware'
+import { authenticateUser } from '../middlewares/authentication.middleware.js'
 import { userController } from '../controllers/user.controller.js'
 
 const routers = express.Router()
