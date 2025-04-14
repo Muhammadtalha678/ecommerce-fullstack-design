10698ae05abafc38ce8fa8ee6d890c12e7d47004
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:06:31 +0500

    export user routers

--- backend-express/src/routers/user.router.js
+++ backend-express/src/routers/user.router.js
@@ -5,3 +5,5 @@ import { userController } from '../controllers/user.controller.js'
 const routers = express.Router()
 
 routers.get('/userInfo', authenticateUser, userController)
+
+export default routers
