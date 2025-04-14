b9f5cea48036765c60b23d3c724fe58a876e62b6
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 19:08:37 +0500

    change prod to next prod

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -1,6 +1,6 @@
 
 const DEV_URL = "http://localhost:5000"
-const PROD_URL = process.env.PROD_URL
+const PROD_URL = process.env.NEXT_PUBLIC_PROD_URL
 const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
 export const ApiRoutes = {
     register: `${BASE_URL}/api/auth/register`,
