9ffe68dde33a630464b5ac4158643343272431f5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 21:13:39 +0500

    add refresh controller

--- backend-express/src/routers/auth.router.js
+++ backend-express/src/routers/auth.router.js
@@ -2,13 +2,13 @@ import express from 'express'
 import { validateRequest } from '../middlewares/validateRequest.middleware.js'
 import {loginValidation, registerValidation} from '../lib/validations/auth.validation.js'
 
-import { loginController, registerController, resendEmailController, verifyEmailController } from '../controllers/auth.controller.js'
+import { loginController, refreshTokenController, registerController, resendEmailController, verifyEmailController } from '../controllers/auth.controller.js'
 const router = express.Router()
 
 router.post('/register', validateRequest(registerValidation), registerController)
 router.post('/login', validateRequest(loginValidation), loginController)
 router.post('/verify-email', verifyEmailController)
 router.post('/resend-verification', resendEmailController)
-router.post('/refreshToken', resendEmailController)
+router.post('/refreshToken', refreshTokenController)
 
 export default router
