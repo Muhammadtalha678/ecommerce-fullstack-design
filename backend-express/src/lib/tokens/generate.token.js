76ce7459058a73969851c525bc8c03708644de8f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 10:53:10 +0500

    add expires token time

--- backend-express/src/lib/tokens/generate.token.js
+++ backend-express/src/lib/tokens/generate.token.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
 import {env} from '../configs/env.config.js'
 const accessToken = (payload) => {
-    return jwt.sign(payload,env.AUTH_SECRET)
+    return jwt.sign(payload,env.AUTH_SECRET,{expiresIn:'15m'})
 }
 const refreshToken = (payload) => {
-    return jwt.sign(payload,env.REFRESH_SECRET)
+    return jwt.sign(payload,env.REFRESH_SECRET,{expiresIn:'7d'})
 }
 export {accessToken,refreshToken}
