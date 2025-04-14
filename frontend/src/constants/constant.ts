e582f2568854ef6e229a3fd8abd6c9e96a0c9fd0
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-14 18:17:11 +0500

    better with template literals

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -3,8 +3,8 @@ const DEV_URL = "http://localhost:5000"
 const PROD_URL = process.env.PROD_URL
 const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
 export const ApiRoutes = {
-    register: BASE_URL + '/api/auth/register',
-    login: BASE_URL + '/api/auth/login',
-    verfiyEmail: BASE_URL + '/api/auth/verify-email',
-    resendEmail: BASE_URL + '/api/auth/resend-verification'
+    register: `${BASE_URL}/api/auth/register`,
+    login: `${BASE_URL}/api/auth/login`,
+    verfiyEmail: `${BASE_URL}/api/auth/verify-email`,
+    resendEmail: `${BASE_URL}/api/auth/resend-verification`
 }
