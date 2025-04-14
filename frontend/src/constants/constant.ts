0ef7d27d06f54f8d9c1876b562398b286f0d1271
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 00:47:56 +0500

    add get headers token method

--- frontend/src/constants/constant.ts
+++ frontend/src/constants/constant.ts
@@ -1,4 +1,4 @@
-
+import Cookies from 'js-cookie'
 const DEV_URL = "http://localhost:5000"
 const PROD_URL = process.env.NEXT_PUBLIC_PROD_URL
 const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL
@@ -8,4 +8,14 @@ export const ApiRoutes = {
     verfiyEmail: `${BASE_URL}/api/auth/verify-email`,
     resendEmail: `${BASE_URL}/api/auth/resend-verification`,
     refreshToken: `${BASE_URL}/api/auth/refreshToken`,
+    user: `${BASE_URL}/api/auth/user`,
+}
+
+export const getHeaders = () => {
+    const token = Cookies.get('token')
+    return ({
+        headers: {
+            Authorization: `Bearer ` + token
+        }
+    })
 }
