a71a61d1c1de2ab54e236f9c7c6ea1ef2c3d7876
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 02:06:01 +0500

    add user controller to handle logics

---
+++ backend-express/src/controllers/user.controller.js
@@ -0,0 +1,10 @@
+import {sendResponse} from '../helpers/sendResponse.js'
+export const userController = async (req, res) => {
+    try {
+        const {fullname,email,role} = req.body
+        sendResponse(res, 200, false, {},{fullname,email,role})
+    } catch (error) {
+        
+        sendResponse(res, 500, true, {general:"Something went wrong"},null)
+    }
+}
