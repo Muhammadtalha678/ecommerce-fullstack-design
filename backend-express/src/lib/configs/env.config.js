9518eff1fdb79c2c94cbf11a3c344ab5891313c7
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 23:48:12 +0500

    add  cloudinary configuration from env

--- backend-express/src/lib/configs/env.config.js
+++ backend-express/src/lib/configs/env.config.js
@@ -8,5 +8,8 @@ export const env = {
     EMAIL_USER:process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
     AUTH_SECRET:process.env.AUTH_SECRET, 
-    REFRESH_SECRET:process.env.AUTH_SECRET 
+    REFRESH_SECRET: process.env.AUTH_SECRET,
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
 }
