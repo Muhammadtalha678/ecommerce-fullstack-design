044a92241fc9b0b138a83ff7888e43402fdb5900
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-16 00:31:26 +0500

    addbanner and detail images

--- frontend/src/components/Admin/ProductForm.tsx
+++ frontend/src/components/Admin/ProductForm.tsx
@@ -6,7 +6,6 @@ import React, { ChangeEvent } from 'react'
 interface FormValues {
     name?: string
     price?: string
-    image?: string
     description?: string
     category?: string
     stock?: string
@@ -22,7 +21,8 @@ const ProductForm = ({ action, state, pending, isEdit = false, FormValues, oncha
         onchange?: (e: ChangeEvent<HTMLInputElement>) => void
     }) => {
     return (
-        <form action={action} className="space-y-4">
+        <form action={action} className="space-y-4" encType="multipart/form-data"
+        >
             <div>
                 <label className="block mb-1 font-medium">Product Name</label>
                 <input
@@ -42,7 +42,7 @@ const ProductForm = ({ action, state, pending, isEdit = false, FormValues, oncha
             <div>
                 <label className="block mb-1 font-medium">Description</label>
                 <input
-                    type="number"
+                    type="text"
                     name="description"
                     value={FormValues?.description}
                     onChange={onchange}
@@ -68,22 +68,6 @@ const ProductForm = ({ action, state, pending, isEdit = false, FormValues, oncha
                 <p className=" text-red-500 text-sm mt-6">
                     {
                         state?.error && state.errors.price && state.errors.price
-                    }
-                </p>
-            </div>
-            <div>
-                <label className="block mb-1 font-medium">Image URL</label>
-                <input
-                    type="text"
-                    name="image"
-                    value={FormValues?.image}
-                    onChange={onchange}
-                    // required
-                    className="w-full border rounded px-3 py-2"
-                />
-                <p className=" text-red-500 text-sm mt-6">
-                    {
-                        state?.error && state.errors.image && state.errors.image
                     }
                 </p>
             </div>
@@ -119,6 +103,32 @@ const ProductForm = ({ action, state, pending, isEdit = false, FormValues, oncha
                     }
                 </p>
             </div>
+
+            {/* File inputs */}
+            <div>
+                <label className="block mb-1 font-medium">Banner Image</label>
+                <input
+                    type="file"
+                    name="bannerImage"
+                    accept="image/*"
+                    required
+                    className="w-full border rounded px-3 py-2"
+                />
+            </div>
+
+            {/* DETAIL IMAGES */}
+            <div>
+                <label className="block mb-1 font-medium">Detail Images (4 max)</label>
+                <input
+                    type="file"
+                    name="detailImages"
+                    accept="image/*"
+                    multiple
+                    required
+                    className="w-full border rounded px-3 py-2"
+                />
+            </div>
+
             <button type="submit" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700" disabled={pending}>
                 {isEdit ? 'Update Product' : pending ? "Processing..." : 'Add Product'}
             </button>
