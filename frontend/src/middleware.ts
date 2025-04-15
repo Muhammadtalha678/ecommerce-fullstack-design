8b6792bbec6d7f2216faa6863b6a75424fb621f2
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 12:13:23 +0500

    middleware complete for protected public and auth routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,42 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { fetchUser } from "./lib/api/fetchUser";
 export async function middleware(request: NextRequest) {
-    const token = request.cookies.get('token')
+    const token = request.cookies.get('token')?.value
     const { pathname } = request.nextUrl
+    // console.log(token);
 
     const authPaths = ['/login', '/register', '/verifyEmail']
-    const protectedPaths = ['/profile', '/cart']
+    const protectedPaths = ['/profile', '/cart', '/admin']
+
 
     if (token && authPaths.includes(pathname)) {
         return NextResponse.redirect(new URL('/', request.url))
     }
+    // console.log(protectedPaths.includes(pathname));
+    if (protectedPaths.includes(pathname)) {
+        if (!token) {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+        try {
+            const userData = await fetchUser(token)
+            console.log(userData);
+
+            if (!userData) {
+                throw new Error('No user data returned');
+            }
+            // Redirect if user is not admin but trying to access admin routes
+            if (pathname.startsWith('/admin') && userData.role !== 'admin') {
+                return NextResponse.redirect(new URL('/', request.url))
+            }
+        } catch (error) {
+            return NextResponse.redirect(new URL('/login', request.url))
+
+        }
+    }
+    return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/login', '/register', '/verifyEmail']
+    matcher: ['/login', '/register', '/verifyEmail', '/profile', '/cart', '/admin/:path*']
 }
 
