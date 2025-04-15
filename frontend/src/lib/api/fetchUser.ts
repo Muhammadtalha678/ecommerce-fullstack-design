a694996585bc59b6664f3e7541dc36270bf1e5dd
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 10:46:56 +0500

    create fetch user api to get user info on every refresh or after login

--- frontend/src/lib/api/fetchUser.ts
+++ frontend/src/lib/api/fetchUser.ts
@@ -1,13 +1,18 @@
 import { ApiRoutes } from '@/constants/constant'
 import Cookie from 'js-cookie'
 export const fetchUser = async (accessToken: string) => {
+    // console.log(accessToken);
+
     try {
         let response = await fetch(ApiRoutes.user, {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
             },
             credentials: "include",
         })
+        console.log(response.status);
+
         if (response.status === 401) {
             const refreshRes = await fetch(ApiRoutes.refreshToken, {
                 method: "POST",
@@ -17,7 +22,11 @@ export const fetchUser = async (accessToken: string) => {
                 const errData = await refreshRes.json()
                 throw new Error(errData.errors.message || "Session expired");
             }
-            const { accessToken: newToken } = await refreshRes.json();
+            const refreshData = await refreshRes.json();
+            const newToken = refreshData.data?.accessToken
+            if (!newToken) {
+                throw new Error('No access token in refresh response');
+            }
             Cookie.set("token", newToken, {
                 expires: 15 / (24 * 60),
                 secure: process.env.NODE_ENV === "production",
@@ -27,17 +36,23 @@ export const fetchUser = async (accessToken: string) => {
             response = await fetch(ApiRoutes.user, {
                 headers: {
                     Authorization: `Bearer ${newToken}`,
+                    'Content-Type': 'application/json',
                 },
                 credentials: "include",
             });
         }
         const data = await response.json();
+        console.log("data=>>>>>>>>>", data);
 
         if (!response.ok) {
-            throw new Error(data.message || "Something went wrong");
+            throw new Error(data.errors?.general || "Something went wrong");
+        }
+        if (!data.data) {
+            throw new Error('No user data in response');
         }
         return data.data;
     } catch (error) {
+        console.log("error=>>>>>>>>>", error);
         const err = error as Error
         throw new Error(err.message || "Unexpected error");
     }
