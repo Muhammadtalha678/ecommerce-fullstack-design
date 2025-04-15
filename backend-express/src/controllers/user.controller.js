4c14937f59b6e3b6e647271c79813291279a1a46
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 12:15:44 +0500

    fetch user data directly from req

--- backend-express/src/controllers/user.controller.js
+++ backend-express/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import {sendResponse} from '../helpers/sendResponse.js'
 export const userController = async (req, res) => {
     try {
-        const {fullname,email,role} = req.body
+        const {fullname,email,role} = req.user
         sendResponse(res, 200, false, {},{fullname,email,role})
     } catch (error) {
         
