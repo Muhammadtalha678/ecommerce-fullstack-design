5b8ec7bfcc980905f7aaa679ef67d286798c4619
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 10:46:33 +0500

    create auth context

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -13,27 +13,46 @@ interface User {
 interface AuthContextType {
     user: User | null,
     isAuthenticated: boolean,
-    loginUser: (token: string) => void
+    loginUser: (token: string) => void,
+    loading: boolean
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
     useEffect(() => {
         const token = Cookie.get('token');
         if (token) {
+            setLoading(true)
             fetchUser(token)
                 .then((userData) => {
-                    setUser(userData!);
+                    if (userData) {
+                        setUser(userData);
+                    }
+                    else {
+                        console.warn('No user data returned');
+                        Cookie.remove('token');
+                    }
                 })
                 .catch((err) => {
-                    toast.error(err?.message || "Session expired or invalid token");
+                    console.error('useEffect fetchUser error:', err.message);
+                    toast.error(err?.message || 'Session expired or invalid token');
                     Cookie.remove('token');
-                });
+                    setUser(null);
+                }).finally(() => {
+                    setLoading(false)
+                }
+                );
+        }
+        else {
+            setLoading(false)
         }
     }, []);
     const loginUser = async (token: string) => {
+        console.log("token==>>>>>>", token);
+        setLoading(true)
         try {
             Cookie.set('token', token, {
                 expires: 15 / (24 * 60), // 15 minutes
@@ -42,13 +61,20 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
             });
 
             const userData = await fetchUser(token);
-            setUser(userData!);
-            // toast.success("Login successful");
-        } catch (err: any) {
-            toast.error(err?.message || "Login failed. Please try again.");
-        }
+            console.log("userData==>>>>>>", userData);
+            if (!userData) {
+                throw new Error('No user data returned');
+            }
+            setUser(userData);
+            toast.success("Login successful");
+        } catch (error) {
+            const err = error as Error
+            toast.error(err?.message || 'Login failed. Please try again.');
+            Cookie.remove('token');
+            setUser(null);
+        } finally { setLoading(false) }
     };
-    return <AuthContext.Provider value={{ user, isAuthenticated: !!user, loginUser }}>
+    return <AuthContext.Provider value={{ user, isAuthenticated: !!user, loginUser, loading }}>
         {children}
     </AuthContext.Provider>
 }
