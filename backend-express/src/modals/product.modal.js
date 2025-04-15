6194597f4a06bf6e3041df1d9972753480aaa0db
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 23:36:49 +0500

    update product modal schema add details images

--- backend-express/src/modals/product.modal.js
+++ backend-express/src/modals/product.modal.js
@@ -3,7 +3,8 @@ import mongoose, { Schema } from 'mongoose'
 const ProductSchema = new Schema({
     name:{type:String,required: true},
     price:{type:String,required: true},
-    image:{type:String,required: true},
+    bannerImage:{type:String,required: true},
+    detailImages:[{type:String}],
     description:{type:String,required: true},
     category:{type:String,required: true},
     stock:{type:String,required: true},
