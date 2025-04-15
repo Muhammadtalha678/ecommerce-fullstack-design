4544d2048fe4cbf80a945fe0dfd34e7ff2308335
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-15 10:46:09 +0500

    import jwt

--- backend-express/src/middlewares/authentication.middleware.js
+++ backend-express/src/middlewares/authentication.middleware.js
@@ -1,13 +1,11 @@
 import { env } from "../lib/configs/env.config.js";
 import {sendResponse} from '../helpers/sendResponse.js'
 import UserModal from '../modals/user.modal.js'
+import jwt from 'jsonwebtoken'
 export async function authenticateUser(req, res, next) {
     try {
         const authHeader = req?.headers?.authorization
         
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return sendResponse(res, 401, true, { general: 'No token provided' }, null);
-        }
         const token = authHeader?.split(' ')[1];
         let decoded;
         try {
@@ -15,11 +13,14 @@ export async function authenticateUser(req, res, next) {
         } catch(error) {
             return sendResponse(res,401,true,{general:"Invalid access token"},null)
         }
-        const user = await UserModal.findById(decoded.id).lean();
+        const user = await UserModal.findById(decoded.id);
+        // console.log(user);
         if (!user) {
         return sendResponse(res, 401, true, { general: 'User not found' }, null);
         }
         req.user = user;
+        // console.log("req.user",req.user);
+        
         next();
     } catch (error) {
         sendResponse(res,500,true,{general:"Something went wrong"},null);
