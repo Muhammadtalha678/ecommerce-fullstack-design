1c7d43511a8214096aa421d6e2b9d0d48f45f9ac
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-16 17:44:24 +0500

    set access token expiration for 1 day

--- backend-express/src/lib/tokens/generate.token.js
+++ backend-express/src/lib/tokens/generate.token.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 import {env} from '../configs/env.config.js'
 const generateAccessToken = (payload) => {
-    return jwt.sign(payload,env.AUTH_SECRET,{expiresIn:'15m'})
+    return jwt.sign(payload,env.AUTH_SECRET,{expiresIn:'1d'})
 }
 const generateRefreshToken = (payload) => {
     return jwt.sign(payload,env.REFRESH_SECRET,{expiresIn:'7d'})
