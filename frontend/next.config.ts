f83addfe0084e368ec6cd1d7e1d54ce1c4b2b00f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-16 22:09:05 +0500

    add images pattern

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '10mb'
     }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+
+      }
+    ]
   }
 };
 
