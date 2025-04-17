3c71b8877b5a14647a7427c4d1c908e41151c666
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-04-17 20:41:35 +0500

    fetch product by id in edit page complete

--- frontend/src/app/(admin)/admin/products/edit/[id]/page.tsx
+++ frontend/src/app/(admin)/admin/products/edit/[id]/page.tsx
@@ -1,10 +1,57 @@
+import ProductForm from '@/components/Admin/ProductForm';
+import { ApiRoutes } from '@/constants/constant';
+import { ApiResponse } from '@/interfaces/Auth';
+import { FetchProductsResult } from '@/interfaces/Product';
 import React from 'react'
+import EditProductComp from './EditProduct';
+const fetchSingleProduct = async (id: string): Promise<FetchProductsResult> => {
+    try {
+        const response = await fetch(`${ApiRoutes.singleProduct}/${id}`)
+        const responseData = await response.json()
+        if (!response.ok) {
+            return {
+                product: null,
+                errors: {
+                    general: responseData.errors.general,
+                },
+            }
+        }
+        const {
+            error, errors, data
+        }: ApiResponse = responseData
 
-const EditProduct = () => {
+        if (error) {
+            return { product: null, errors: { general: errors.general } }
+        }
+
+        return { product: data?.foundProduct, errors: null }
+    } catch (error) {
+        const err = error as Error
+        return { product: null, errors: { general: err.message } }
+
+    }
+}
+const EditProduct = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params
+    const { errors, product } = await fetchSingleProduct(id)
+
+    if (errors) {
+        return (
+            <div className="flex flex-col items-center justify-center my-6 text-center">
+                <h1 className="text-2xl font-bold text-red-600">Something went wrong!</h1>
+                <p className="text-gray-600">{errors.general}</p>
+            </div>
+        )
+    }
+    if (!product) {
+        return (
+            <h1 className="md:text-3xl text-2xl text-black font-extrabold mb-5 text-center my-5">
+                No Product Found
+            </h1>
+        )
+    }
     return (
-        <div>
-
-        </div>
+        <EditProductComp product={product} />
     )
 }
 
